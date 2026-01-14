d47d19758f50c0c0ad0f8583f4fae69a66fefef5
Dmitry Shashkov (Brookit) <brookit18@gmail.com>
2026-01-14 22:11:54 +0300

    [fix]: Изменено название процедуры вывода сообщения

--- src/main/kotlin/its/model/definition/loqi/tree/DebugPointDef.kt
+++ src/main/kotlin/its/model/definition/loqi/tree/DebugPointDef.kt
@@ -3,7 +3,7 @@ package its.model.definition.loqi.tree
 import its.model.definition.types.StringType
 
 class DebugPointDef(): CallableProcedureDef(
-    "print",
+    "point",
     listOf(ProcedureArgument("comment", StringType)),
     scopeCapture = false
 )
